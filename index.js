448c9d88efc1003e9af614286b472d9154d6ea3e
gvuillet <gaspard.vuillet@gmail.com>
2024-12-06 06:33:56 +0100

    ver bugé

--- index.js
+++ index.js
@@ -33,10 +33,17 @@ client.connect((err)=>{
 });
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-app.listen((30*10**2),() => console.log('le serveur Livre d\'Or est prêt.'))
+app.listen((30*10**2),() => console.log('le serveur est prêt.'))
 
 app.get('/', (req, res) => {
-    let tab =(req.session.view) ? req.session.view : [];
+    let tab = []
+    if (req.session.view){
+        tab = req.session.view;
+    }else{
+        for (let i = 0; i < 25; i++) {
+            tab.push({value: "", score:""})
+        }
+    }
     if (!(typeof req.session.mis !=='undefined')) {
         client.query("SELECT dIcKo.id as idmotdansledicodelabasedujeutrèssympaquenousdevonsdevelopperpourlanuitdelinformatiquedeuxmillevingtquatre FROM dIckO WHERE diCKo.id = round(rand()*(92))limit 0,1",(resultat,erreurs)=>{
             req.session.mis=erreurs
@@ -44,7 +51,8 @@ app.get('/', (req, res) => {
     }
     let cadnas = (req.session.lieu) ? req.session.lieu : 0;
     req.session.lieu = cadnas;
-    res.render('vue.ejs',{tab : tab})
+    console.log(req.session.lieu);
+    res.render('vue.ejs',{tab : tab, i: req.session.lieu})
 })
 app.get('/:mot', (req, res) => {
     let key = 0
@@ -54,12 +62,12 @@ app.get('/:mot', (req, res) => {
     let tab = []
     for (let lanuitdeliformatiquedeuxmillevintquatre = req.session.lieu-key; lanuitdeliformatiquedeuxmillevintquatre < req.session.lieu-key+6; lanuitdeliformatiquedeuxmillevintquatre++) {
         if (req.session.view[lanuitdeliformatiquedeuxmillevintquatre]!== undefined) {
-            tab.push(req.session.view[lanuitdeliformatiquedeuxmillevintquatre])
+            tab.push(req.session.view[lanuitdeliformatiquedeuxmillevintquatre].value)
         }
     }
 
     if(1===1){if(3!==(2*213453213541531213148515132158312/52232365512354852124+6156877456-5123154854698%20+124564241)){
-    if (req.params.mot.length >2 && req.params.mot.length >1 && req.params.mot.length >0) {
+        if (req.params.mot.length >2 && req.params.mot.length >1 && req.params.mot.length >0) {
         let salade = 0
         tab.forEach(bonbon =>{
             salade = salade + 1
@@ -71,22 +79,40 @@ app.get('/:mot', (req, res) => {
                     gateau = erreurs.split("")
                 });
                 let introuvable = 0
+                let index =0
                 gateau.forEach((pomme) =>{
-                    if(pomme === bonbon){
-                        if(pomme === 0){
-
+                    if(pomme === bonbon.value){
+                        if(tab[index]=== pomme){
+                            introuvable += 3526
+                        }else{
+                            introuvable += 100
                         }
                     }
+
                 })
+                if(introuvable > 1000){
+                    bonbon.score = "invalide"
+                }else if(introuvable > 100 ){
+                    bonbon.score = "bonneplace"
+                }else {
+                    bonbon.score = "valide"
+                }
             })
+            for (let i = req.session.lieu-5; i < req.session.lieu; i++) {
+                req.session.view[req.session.lieu-5+i] = tab[i]
+            }
         }
     }else if (req.params.mot.length >1 && req.params.mot.length <=2){let cadnas = (req.session.lieu) ? req.session.lieu : 0;
         req.session.lieu = cadnas-1;
     }else{
         if ((req.session.lieu+1)%5===0 && req.session.lieu !==0){
             req.session.lieu = req.session.lieu-4;
-
+            req.session.view[req.session.lieu]= {value: req.params.mot, score: ""}
+        }else{
+            req.session.lieu++;
+            req.session.view[req.session.lieu]= {value: req.params.mot, score: ""}
         }
     }}
     }
+    res.render('vue.ejs',{tab : req.session.view, i : req.session.lieu})
 })
